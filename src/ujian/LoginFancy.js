f18457ab8f33e0a55164104418c2d7b42258197b
NeoFreya <neonjake019@gmail.com>
2023-01-23 13:43:33 +0700

    fancy notif

---
+++ src/ujian/LoginFancy.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginFancy = () => {
+  return (
+    <View>
+      <Text>LoginFancy.</Text>
+    </View>
+  )
+}
+
+export default LoginFancy
+
+const styles = StyleSheet.create({})
